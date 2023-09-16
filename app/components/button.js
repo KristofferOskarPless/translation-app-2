3b05d1a926f1414200e0faef6407499d8cddd395
KristofferOskarPless <pless@ursolutions.no>
2023-09-16 11:49:08 +0200

    add frontend

---
+++ app/components/button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Button() {
+  return (
+    <button
+      className="w-full mt-8 bg-emerald-700 hover:bg-emerald-800 text-white py-5 px-4 rounded-md"
+    >
+      Check answer
+    </button>
+  );
+}
